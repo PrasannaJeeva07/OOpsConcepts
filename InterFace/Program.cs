523b43b2dbb3e7df8ceadc3155acacf159e90015
Jeeva-Vedha <jeeva.vedaparaki@defisolutions.com>
2023-07-16 18:18:16 +0530

    Added Interface Example

---
+++ InterFace/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace InterFace
+{
+    interface Dealers
+    {
+        void GetDealers();
+    }
+    class Program : Dealers
+    {
+        static void Main(string[] args)
+        {
+            Dealers d = new Program();
+            d.GetDealers();
+            Console.ReadLine();
+        }
+
+        public void GetDealers()
+        {
+            Console.WriteLine("This is from Derived Class Dealer");
+            
+        }
+    }
+}
